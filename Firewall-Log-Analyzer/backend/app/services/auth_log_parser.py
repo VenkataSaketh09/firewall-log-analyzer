db1153c777cc6836d862a0107897b2df2ab4243c
VenkataSaketh09 <mvsaketh2020@gmail.com>
2025-12-26 15:49:56 +0530

    created simple log ingestion function  for auth and ufw logs and stored in DB

---
+++ Firewall-Log-Analyzer/backend/app/services/auth_log_parser.py
@@ -0,0 +1,46 @@
+import re
+from datetime import datetime
+from app.models.log_model import build_log
+
+FAILED_REGEX = re.compile(
+    r"Failed password for (invalid user )?(?P<user>\w+) from (?P<ip>[\d.]+)"
+)
+
+SUCCESS_REGEX = re.compile(
+    r"Accepted password for (?P<user>\w+) from (?P<ip>[\d.]+)"
+)
+
+def parse_auth_log(line: str):
+    timestamp = datetime.utcnow()
+
+    if "Failed password" in line:
+        match = FAILED_REGEX.search(line)
+        if match:
+            return build_log(
+                timestamp=timestamp,
+                source_ip=match.group("ip"),
+                destination_port=22,
+                protocol="TCP",
+                log_source="auth.log",
+                event_type="SSH_FAILED_LOGIN",
+                severity="HIGH",
+                username=match.group("user"),
+                raw_log=line.strip()
+            )
+
+    if "Accepted password" in line:
+        match = SUCCESS_REGEX.search(line)
+        if match:
+            return build_log(
+                timestamp=timestamp,
+                source_ip=match.group("ip"),
+                destination_port=22,
+                protocol="TCP",
+                log_source="auth.log",
+                event_type="SSH_LOGIN_SUCCESS",
+                severity="LOW",
+                username=match.group("user"),
+                raw_log=line.strip()
+            )
+
+    return None
